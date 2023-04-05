1c8ba9af4579cedbeb4631a80b20e89b69ecddcc
kjhto46 <tony1996kjh@gmail.com>
2023-04-05 22:40:36 +0900

    동영상 삭제하기

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,6 +4,7 @@ import {
   getUpload,
   getEdit,
   postEdit,
+  deleteVideo,
   postUpload,
 } from "../controllers/videoController";
 
@@ -11,6 +12,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
