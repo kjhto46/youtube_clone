6b13ee71384df9d82af4d6f752c2d3eef4c286a7
kjhto46 <tony1996kjh@gmail.com>
2023-04-06 22:29:45 +0900

    검색기능

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,7 +9,7 @@ console.log("finished")
 */
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -70,4 +70,18 @@ export const deleteVideo = async (req, res) => {
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 
+};
+
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
