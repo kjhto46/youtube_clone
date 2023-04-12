cb4193d01088cc056761698519bad6c0327fcf50
kjhto46 <tony1996kjh@gmail.com>
2023-04-12 21:54:27 +0900

    아이디 비밀번호 정상 로그인 설정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -39,16 +40,24 @@ export const getLogin = (req, res) =>
 
 export const postLogin = async (req, res) => {
     const { username, password } = req.body;
-    const exists = await User.exists({ username });
-    if (!exists) {
+    const pageTitle = "Login";
+    const user = await User.findOne({ username });
+    //계정이 존재하는지 체크
+    if (!user) {
         return res.status(400).render("login", {
-            pageTitle: "Login",
+            pageTitle,
             errorMessage: "An account with this Username does not exists."
         });
     }
-    //계정이 존재하는지 체크
     //패스워드가 일치하는지 채크
-    res.end();
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "Wrong password.",
+        })
+    }
+    return res.redirect("/");
 };
 
 export const edit = (req, res) => res.send("Edit User");
