6539977bf74e7ee3b237b57ca99cd250276383e8
kjhto46 <tony1996kjh@gmail.com>
2023-04-15 21:18:26 +0900

    session을 이용한 쿠기 기록 카운트

--- src/server.js
+++ src/server.js
@@ -3,26 +3,38 @@ import morgan from "morgan";
 import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
-import userRounter from "./routers/userRouter";
+import userRouter from "./routers/userRouter";
 
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine", "pug")
-app.set("views", process.cwd() + "/src/views")
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    session({
-        secret: "Hello!",
-        resave:true,
-        saveUninitialized:true,
-    })    
-)
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/", rootRouter)
-app.use("/users", userRounter)
-app.use("/videos", videoRouter)
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
 
-export default app
+app.get("/add-one", (req, res, next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
+app.use("/", rootRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+export default app;
