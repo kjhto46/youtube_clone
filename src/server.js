2b3d5f9c3494a5bb79403a628698fb633f7020d3
kjhto46 <tony1996kjh@gmail.com>
2023-05-02 23:00:06 +0900

    server.js에 express.static()로 정적으로 불러오기

--- src/server.js
+++ src/server.js
@@ -36,6 +36,7 @@ app.use(
 
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads")); //uploads라는 라우터를만들고 express.static() 함수는 정적 파일을 제공하는 미들웨어 함수입니다. express.static() 함수를 사용하면 Express.js 애플리케이션에서 정적 파일(이미지, CSS, JavaScript 파일 등)을 서비스할 수 있습니다. 
+app.use("/static", express.static("assets"));
 //위에 내용을 쉽게 말 하자면 'express로 누군가 /uploads로 가려고 한다면 uploads 폴더의 내용을 보여주라' 라는 뜻
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
