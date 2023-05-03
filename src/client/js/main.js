23e34c83f4bf6b5047c538cb6e4c0c94f8662bc2
kjhto46 <tony1996kjh@gmail.com>
2023-05-03 22:26:55 +0900

    scss, webpack에서 변환하는 법,  MiniCssExtractPlugin로 filename  지정하기

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,5 +1,2 @@
-const hello = async () => {
-   alert("안녕!");
-   const x = await fetch("");
-}
-hello();
+import "../scss/styles.scss"
+console.log("hi");
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
    entry: "./src/client/js/main.js", //entry : "경로에 있는" 파일을 가져오고
    mode: "development",
+   plugins: [
+      new MiniCssExtractPlugin({
+         filename: "css/styles.css",
+      })
+   ],
    output: {
-      filename: "main.js", //파일 이름은 이것
-      path: path.resolve(__dirname, "assets", "js"), // 변환되서 저장되는 폴더는 이곳
+      filename: "js/main.js", //파일 이름은 이것
+      path: path.resolve(__dirname, "assets"), // 변환되서 저장되는 폴더는 이곳
    },
    module: {
       rules: [{
@@ -20,6 +26,9 @@ module.exports = {
                ]
             }
          }
+      }, {
+         test: /\.scss$/,
+         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], //webpack은 역순으로 시작되기때문에 역순으로 작성함
       }]
    }
 }
