abd1d3be2d1e714784fae1e78ddca1d2f8bf973c
kjhto46 <tony1996kjh@gmail.com>
2023-05-09 22:47:10 +0900

    재생 일시정지

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,28 @@
-console.log("videoplayer")
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+   //비디오가 시작중이라면 멈추고
+   if(video.paused) {
+      video.play();
+   } else {
+      playBtn.innerText = "Play";
+      video.pause();
+   }
+   //아니면 재생해라
+}
+
+const handleMute = (e) => {
+
+}
+
+const handlePause = () => {playBtn.innerText = "Play";}
+const handlePlay = () => {playBtn.innerText = "Paused";}
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
