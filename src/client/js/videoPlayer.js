f5bebaf9b8b520c137f9820d9824b6bae910fce2
kjhto46 <tony1996kjh@gmail.com>
2023-05-11 21:57:31 +0900

    일시정지 재생 음소거 기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
    //비디오가 시작중이라면 멈추고
@@ -12,11 +12,20 @@ const handlePlayClick = (e) => {
       playBtn.innerText = "Play";
       video.pause();
    }
+   playBtn.innerText = video.paused ? "Play" : "Pause";
    //아니면 재생해라
 }
 
 const handleMute = (e) => {
-
+   if(video.muted) {
+      video.muted = false;
+      // muteBtn.innerText = "mute"
+   }else{
+      video.muted = true;
+      // muteBtn.innerText = "Unmute"
+   }
+   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+   volumeRange.value = video.muted ? 0 : 0.5;
 }
 
 const handlePause = () => {playBtn.innerText = "Play";}
@@ -24,5 +33,3 @@ const handlePlay = () => {playBtn.innerText = "Paused";}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
