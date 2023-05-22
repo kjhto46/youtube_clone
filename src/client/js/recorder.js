d57a6a3d6194176dae0998256ed11e408116fe4c
kjhto46 <tony1996kjh@gmail.com>
2023-05-22 23:39:42 +0900

    수정

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,13 @@
 const startBtn = document.getElementById("startBtn");
 
-const handleStart = async() => {
+const handleStart = async () => {
    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
+     audio: false,
+     video: true,
    });
-   console.log(stream);
-}
+   video.srcObject = stream;
+   video.play();
+ };
 
 
 startBtn.addEventListener("click", handleStart);
