420fd238ef1278f7a020365dd6b61f584fc9afea
kjhto46 <tony1996kjh@gmail.com>
2023-06-24 06:54:40 +0900

    오류돌리기

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,24 +1,10 @@
 import multer from "multer";
-import multerS3 from "multer-s3";
-import aws from "aws-sdk";
 
-const s3 = new aws.S3({
-    credentials: {
-        accessKeyId: process.env.AWS_ID,
-        secretAccessKey: process.env.AWS_SECRET,
-    },
-});
-
-const multerUploader = multerS3({
-    s3: s3,
-    bucket: 'kjhwetube',
-    acl: 'public-read',
-})
 
 export const localsMiddleware = (req, res, next) => {
     // if(req.session.loggedIn){
     //     res.locals.loggedIn = true
-    // } 
+    // }
     res.locals.loggedIn = Boolean(req.session.loggedIn); //Boolean() req.session.loggedIn 값이 확인되면 (true) (로그인되있다면) request를 계속한다.
     res.locals.siteName = "wetube";
     res.locals.loggedInUser = req.session.user || {}; //|| 는 알다시피 or이라는 뜻 session.user가 없으면 {} 빈공간이다 라는 의미
@@ -47,14 +33,12 @@ export const avatarUpload = multer({
     dest: "uploads/avatars/",
     limits: {
         fileSize: 3000000,
-    },
-    storage: multerUploader,
+    }
 }); //multer라는 middleware로 이미지에 대한 정보를 저장
 
 export const videoUpload = multer({
     dest: "uploads/videos/",
     limits: {
         fileSize: 20000000,
-    },
-    storage: multerUploader,
+    }
 });
